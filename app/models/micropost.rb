73058edd0afa64b16bbe71faec56bcf28543f7da
Takahito Nakashima <19941015takahito@gmail.com>
2018-01-05 13:31:02 +0000

    Modify toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :contents, length: { maximum: 140 },
+  validates :content, length: { maximum: 140 },
                        presence: true
 end
